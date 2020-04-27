5e145fd0556890e3409c5365d629131402784e77
adadgio <romain.bruckert.esc@gmail.com>
2020-04-27 12:40:10 +0200

    - Correction du tree builder.

--- index.php
+++ index.php
@@ -12,12 +12,19 @@ $input = [
     '1. Dénomiation du médicament',
     '2. Effets indésirables',
     '2.1. Effet sur la grossesse',
+    '2.1.1. Effets bruts',
+    '2.1.2. Effets simples',
+    '2.2. Effet sur le travail',
     '3. Forme pharmaceutique',
-    '3.1. Contre indications',
+    '3.1. Contres indications',
+    '3.1.1. Pour indicatons',
     '4. Données pharmaceutiques',
     '4.1. Données de sécurité préclinique'
 ];
 
 $tree = new TreeBuilder($input);
 $tree->build();
+
+echo '<pre>';
 print_r($tree->getTree());
+echo '</pre>';
--- src/TreeBuilder.php
+++ src/TreeBuilder.php
@@ -5,6 +5,7 @@ namespace App;
 class TreeBuilder
 {
     private $flatData = [];
+    private $itemsArranged = [];
     private $tree = [];
 
     public function __construct($flatData = [])
@@ -16,10 +17,77 @@ class TreeBuilder
     {
         // fill in this method
         // you can create other methods to organize your code.
+        $nLevels = 0;
+        $leveledData = [];
+        $data = [];
 
         // @FILLTHIS
+        foreach ($this->flatData as $row) {
+            $info = self::info($row);
 
+            $this->itemsArranged[] = [
+                'row' => $row,
+                'numid' => $info['numid'],
+                'level' => $info['level'],
+                'parent' => $info['parent'],
+            ];
+        }
+
+        $this->tree = $this->mapTree($this->itemsArranged, null, 1);
+        
         return $this;
+    }
+
+    private function mapTree(array $items, $parent = null, $level = 1)
+    {
+        $tree = [];
+
+        foreach ($items as $k => $item) {
+
+            if ($level !== $item['level']) {
+                continue;
+            }
+
+            $tree[$k] = $item;
+            $tree[$k]['children'] = [];
+
+            $children = self::findChildrenIn($this->itemsArranged, $item['numid']);
+            $tree[$k]['children'] = $this->mapTree($children, $item, ($level + 1));
+        }
+
+        // could sort tree as well...
+        return array_values($tree);
+    }
+
+    public static function findChildrenIn($list, $numid)
+    {
+        $children = [];
+
+        foreach ($list as $item) {
+            if ($item['parent'] === $numid) {
+                $children[] = $item;
+            }
+        }
+
+        return $children;
+    }
+
+    public static function info($row)
+    {
+        $parts = explode('.', $row); // 2 , 1
+        $level = count($parts) - 1;
+        $nums = array_splice($parts, 0, count($parts) - 1);
+        $label = $parts[count($parts) - 1];
+
+        $parent = null;
+        $numid = implode('.', $nums);
+
+        if (count($nums) > 1) {
+            $pnums = array_splice($nums, 0, count($nums) - 1);
+            $parent = implode('.', $pnums);
+        }
+
+        return [ 'level' => $level, 'numid' => $numid, 'label' => $label, 'parent' => $parent];
     }
 
     public function getTree()
